3951958834740797ec54ec643dbec528d92e7410
Neel Mishra <neelmishra9125@gmail.com>
2025-03-03 10:12:51 +0530

    navbar done

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const scroll = new LocomotiveScroll({
 
 var tl = gsap.timeline()
 
-tl.to('#page1',{
+tl.to('#wrapper',{
     y:'100vh',
     scale:0.6,
     duration:0,
 })
-tl.to("#page1",{
+tl.to("#wrapper",{
     y:"30vh",
     duration:1,
     delay:1,
 })
-tl.to("#page1",{
+tl.to("#wrapper",{
     y:"0vh",
     rotate:360,
     scale:1,
